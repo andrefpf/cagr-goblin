20cb3d2af77ef8d6b3a9789703f53bd3b0d97eb6
André Fernandes <fpf.andre@gmail.com>
2023-08-12 18:12:37 -0300

    refactor graph generator

--- curricufsc/graph_generator.py
+++ curricufsc/graph_generator.py
@@ -1,70 +1,99 @@
 import graphviz
 import json
 import re
+from collections import defaultdict
 
 
 SUBJECT_CODE_REGEX = re.compile("[A-Z]{3}[0-9]{4}")
 
+# The order may appear chaotic (and it is)
+# but it makes the output much prettier
+VALID_COLORS = ["4", "5", "6", "2", "1"]
 
-layout = '''
+
+LAYOUT = '''
     // direction of graph layout is left to right
-    rankdir=LR;
+    rankdir=LR
 
     // edges route around nodes with polygonal chains
-    splines=ortho;
+    splines=ortho
 
     // set transparent background
-    bgcolor="#00000000";
+    bgcolor="#00000000"
 
     // set global style for nodes
     node [
     width=1.4 height=.5 shape=box style=filled
     fontname=Arial colorscheme=set36
-    ];
+    ]
 
     // set global style for edges
-    edge [style=bold colorscheme=set36 arrowsize=.5 arrowhead=tee];
+    edge [style=bold colorscheme=set36 arrowsize=.5 arrowhead=tee]
 
 '''
 
 
-with open("data/curriculos_json/ciencia_computacao.json") as file:
-    curriculum_data = json.load(file)
+class GraphGenerator:
+    def __init__(self, path):
+        self.curriculum_data = self.load_data(path)
 
-# for subjects in curriculum_data.values():
-#     subjects.sort(key=lambda x: x["codigo"], reverse=True)
+        # small hack that increments the counter for new values
+        self.color_counter = defaultdict(lambda: len(self.color_counter))
 
-g = graphviz.Digraph("grafo_materias")
-g.body.append(layout)
-
-with g.subgraph(name="cluster_everything") as cluster_everything:
-    cluster_everything.attr(color="#00000000")
+    def load_data(self, path):
+        with open(path) as file:
+            curriculum_data = json.load(file)
+        return curriculum_data
     
-    with cluster_everything.subgraph(name="cluster_header") as cluster_header:
-        all_fases = list(curriculum_data.keys())
-        for a, b in zip(all_fases, all_fases[1:]):
-            cluster_header.edge(a, b, style="invis")
+    def generate_graph(self) -> graphviz.Digraph:
+        graph = graphviz.Digraph("grafo_materias")
+        graph.body.append(LAYOUT)
 
-    for i, (fase, subjects) in enumerate(curriculum_data.items()):
-        if "fase" not in fase.lower():
-            continue 
+        # makes everything well aligned
+        with graph.subgraph(name="cluster_everything") as cluster:
+            cluster.attr(color="#00000000")
+            self._create_fase_headers(cluster)
+            self._create_fase_columns(cluster)
 
-        with cluster_everything.subgraph(name=f"cluster_{i}") as cluster:
+        self._create_connections(graph)
+        return graph
+    
+    def _create_fase_headers(self, graph):
+        with graph.subgraph(name="cluster_header") as cluster:
+            all_fases = list(self.curriculum_data.keys())
+            for a, b in zip(all_fases, all_fases[1:]):
+                cluster.edge(a, b, style="invis")
+
+    def _create_fase_columns(self, graph):
+        for i, subjects in enumerate(self.curriculum_data.values()):
+            with graph.subgraph(name=f"cluster_{i}") as cluster:
+                for subject in subjects:
+                    subject_code = subject["codigo"]
+                    color = self._get_subject_color(subject_code)
+                    cluster.node(subject_code, color=color)
+
+    def _create_connections(self, graph):
+        all_subjects_code = set()
+        for subjects in self.curriculum_data.values():
             for subject in subjects:
-                subject_code = subject["codigo"]
-                cluster.node(subject_code, color="5")
+                all_subjects_code.add(subject["codigo"])
 
-all_subjects = set()
-for subjects in curriculum_data.values():
-    for subject in subjects:
-        all_subjects.add(subject["codigo"])
+        for subjects in self.curriculum_data.values():
+            for subject in subjects:
+                for prerequisite in SUBJECT_CODE_REGEX.findall(subject["pre_requisito"]):
+                    if prerequisite in all_subjects_code:
+                        color = self._get_subject_color(subject["codigo"])
+                        graph.edge(prerequisite, subject["codigo"], color)
 
-for fase, subjects in curriculum_data.items():
-    if "fase" not in fase.lower():
-        continue 
+    def _get_subject_color(self, subject):
+        department_code = subject[:3]
+        if department_code not in self.color_counter:
+            print(department_code)
+        index = self.color_counter[department_code] % len(VALID_COLORS)
+        return VALID_COLORS[index]
 
-    for subject in subjects:
-        for prerequisite in SUBJECT_CODE_REGEX.findall(subject["pre_requisito"]):
-            if prerequisite in all_subjects:
-                g.edge(prerequisite, subject["codigo"])
-g.view()
+
+if __name__ == "__main__":
+    gg = GraphGenerator("data/curriculos_json/ciencia_computacao.json")
+    g = gg.generate_graph()
+    g.view()
